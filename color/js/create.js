09bf225d39093d3be787984712aa1505475c2d62
nicholasramdin <31458944+nicholasramdin@users.noreply.github.com>
2019-11-20 20:43:28 -0500

    added color.css files
    color.css files affect only publish.html at the moment

---
+++ color/js/create.js
@@ -0,0 +1,40 @@
+const loginButton = document.getElementById("login-button");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const passwordrptInput = document.getElementById("passwordrpt");
+const usernameInput = document.getElementById("username");
+const message = document.getElementById("login-message");
+const signupButton = document.getElementById("signup");
+const errormessage = document.getElementById("error-message");
+
+signupButton.onclick = function() {
+    const promise =
+    firebase.auth().createUserWithEmailAndPassword(emailInput.value,passwordInput.value)
+        promise.catch(function(error) {
+ 
+  errorMessage.textContent = error.message;
+  
+});
+    promise.then(function(credential){
+        createUser(credential.user.uid);
+    })
+};
+
+function createUser(id) {
+    
+    const db = firebase.database();
+    const ref = db.ref("users").child(id);
+    ref.update({
+        diaplayName: userInput.value
+    })
+    
+    promise.then(function(){
+                 location.href= "index.html";
+                 
+                 })
+}
+
+
+
+
+
